a6d39b92b4728aa691f6be64ae5c0b3697912155
Chwakillah <syakillahnachwa081@gmail.com>
2024-04-30 13:23:23 +0700

    feat(planet): membuat fitur penjelasan tiap planet

--- src/script.js
+++ src/script.js
@@ -1,14 +1,20 @@
-const toggleNavButton = document.getElementById('toggleNav');
-        const menuIcon = document.getElementById('menuIcon');
-        const navMenu = document.getElementById('navMenu');
+// const toggleNavButton = document.getElementById('toggleNav');
+//         const menuIcon = document.getElementById('menuIcon');
+//         const navMenu = document.getElementById('navMenu');
 
-        toggleNavButton.addEventListener('click', function() {
-            navMenu.classList.toggle('hidden');
-            if (menuIcon.classList.contains('menu')) {
-                menuIcon.classList.remove('menu');
-                menuIcon.classList.add('close');
-            } else {
-                menuIcon.classList.remove('close');
-                menuIcon.classList.add('menu');
-            }
-        });
+//         toggleNavButton.addEventListener('click', function() {
+//             navMenu.classList.toggle('hidden');
+//             if (menuIcon.classList.contains('menu')) {
+//                 menuIcon.classList.remove('menu');
+//                 menuIcon.classList.add('close');
+//             } else {
+//                 menuIcon.classList.remove('close');
+//                 menuIcon.classList.add('menu');
+//             }
+//         });
+
+let toggle = false;
+    function Menu() {
+        let list = document.querySelector('.nav-link');
+        list.classList.toggle('top-[84px]');
+      }
